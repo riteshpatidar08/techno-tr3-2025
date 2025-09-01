2ee5672ac46b806d4fd79cde89836cc85e2c1b79
ritesh <riteshpatidar088@gmail.com>
2025-09-01 10:14:18 +0530

    added js section

---
+++ javascript/script.js
@@ -0,0 +1,29 @@
+console.log('external js file')
+
+//TODO -- WHAT IS JS ?
+//NOTE -- JS IS HIGH LEVEL INTERPRETED LANGUAGE , DYNMAICALLY TPYED , SINGLE THREADED LANGUAGE ;
+
+//TODO - VARIABLE AND VALUE ;
+//TODO - KEYWORDs VAR , LET , CONST ;
+// movieTitle =  'WAR 2' ; ❌
+// console.log(movieTitle)
+
+//NOTE LET 
+
+//NOTE NAMING OF THE VARIABLES :
+
+//NOTE CAMEL-CASE 
+// firstname
+//camelCase => firstName;
+//camelCase => isjsfun;
+//camelCase => isJsFun;
+
+// let 123number = 12  ❌;
+// let _private = 34;
+
+//NOTE LET  //REASSIGN ✔️ AND REDECLARATION 
+let collegeName = 'TECHNO' ;
+collegeName = 'JECRC';
+console.log(collegeName) ;
+
+// let collegeName = 'GIT'  ❌;
