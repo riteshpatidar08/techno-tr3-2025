c3132dbabe2576b8ae1c238d0e417dd325cbd61b
ritesh <riteshpatidar088@gmail.com>
2025-09-01 10:36:30 +0530

    updated till datatypes

--- javascript/script.js
+++ javascript/script.js
@@ -21,9 +21,47 @@ console.log('external js file')
 // let 123number = 12  ❌;
 // let _private = 34;
 
-//NOTE LET  //REASSIGN ✔️ AND REDECLARATION 
+//NOTE LET  //REASSIGN ✔️ AND REDECLARATION  ❌
 let collegeName = 'TECHNO' ;
 collegeName = 'JECRC';
 console.log(collegeName) ;
 
 // let collegeName = 'GIT'  ❌;
+
+//NOTE CONST   REASSIGN  ❌  AND REDECLARATION  ❌
+
+const PI = 3.14 ;
+// const firstName ; ❌
+// PI = 4 ; ❌ 
+// console.log(PI) ❌
+// const PI = 3.14'  ❌
+
+//NOTE VAR   REASSIGN  ✔️  AND REDECLARATION  ✔️ ;
+var  movieTitle = 'WAR' ;
+// movieTitle = 'WAR2' ; ✔️
+// var movieTitle = 'WAR3'; ✔️
+
+
+//NOTE DATATYPES ;
+
+//NOTE PRIMITIVE ;
+//STRING  , NUMBER , BOOLEAN , NULL , UNDEFINED , SYMBOL , BIGINT ;
+
+const firstName =  'TEST' ;
+
+console.log(typeof firstName) 
+
+const phoneNumber = 3424432 ;
+console.log(typeof phoneNumber) ;
+
+const isJsFun = true ;
+console.log(typeof isJsFun) ;
+
+//NOTE UNDEFINED ;
+let product ;
+console.log(product);
+console.log(typeof product);
+
+//NOTE Null
+let company = null;
+console.log(typeof company)
