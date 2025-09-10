5ece007cc5664f6fe5c4ecd58bdceb1bd9acbfb2
ritesh <riteshpatidar088@gmail.com>
2025-09-10 11:00:34 +0530

    updated till events

--- javascript/Day08 10-09/script.js
+++ javascript/Day08 10-09/script.js
@@ -54,13 +54,47 @@ const userData = [
     email: 'test1@gmail.com',
     designation: 'Support Engineer',
     profileUrl: 'https://myprofile.in',
-    avatar : 'https://placehold.co/100x100'
+    avatar: 'https://placehold.co/100x100',
   },
   {
     name: 'test2',
     email: 'test2@gmail.com',
     designation: 'Mern stack engineer',
     profileUrl: 'https://profile.in',
-        avatar : 'https://placehold.co/100x100'
+    avatar: 'https://placehold.co/100x100',
   },
 ];
+const wrapperDiv = document.getElementById('wrapper');
+
+userData.forEach((el) => {
+  const container = document.createElement('div');
+  container.innerHTML = `<h1 id='heading'>${el.name}</h1> <p>${el.email}</p> `;
+  wrapperDiv.append(container);
+});
+
+const list = document.getElementById('list');
+const listItem = document.getElementById('li_third');
+
+const newli = document.createElement('li');
+newli.textContent = 'item2';
+console.log(newli);
+
+list.insertBefore(newli, listItem);
+list.removeChild(listItem); //child remove from the parent
+
+headingFirst.remove(); //individual element removing
+
+function buttonClick() {
+  if (document.body.style.backgroundColor === 'red') {
+    document.body.style.backgroundColor = '';
+  } else {
+    document.body.style.backgroundColor = 'red';
+  }
+}
+
+const btn = document.getElementById('btn');
+console.log(btn.onclick);
+
+btn.onclick = function(){
+    console.log('runnnning.........')
+}
