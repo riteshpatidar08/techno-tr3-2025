334cf53ece6fb11e35bd13e924837e3ef5974ecb
ritesh <riteshpatidar088@gmail.com>
2025-09-11 11:37:55 +0530

    updated till fetch

--- javascript/Day09 11-09/script.js
+++ javascript/Day09 11-09/script.js
@@ -44,3 +44,60 @@ btn.addEventListener('click', () => {
 //Note create a drop down using js , dropdown should be open when i enter the mouse on it.
 //Note create a modal using js.
 //Note create a dark and light theme card ui.
+//NOTE TABLE using js
+
+//ASYNCRONOUS JAVASCRIPT :
+
+//synchronouse code blocking code ;
+// console.log('helllo');
+// alert('this is alert')
+// console.log('hi');
+
+//async code ;
+
+console.log('1');
+//NOTE async code
+setTimeout(() => {
+  console.log('settimeout is running');
+}, 4000); //callback , timer
+console.log(window);
+
+//NOTE async code
+const intervalId = setInterval(() => {
+  console.log('this will run after each 2 sec');
+}, 2000);
+
+// clearInterval(intervalId);
+
+//NOTE EVENT LOOP , CALLBACK QUEUE , CALLSTACK => JS EXECUTION :
+//NOTE ASYNC CODE PATTERN => CALLBACK , PROMISES , SETTIMEOUT , SETINTERVAL
+
+//NOTE PROMISES => promises is a js object which acts like a placeholder for the completion result of async code ;
+
+//PROMISE STATES => PENDING , FULFILLED AND REJECTED ;
+
+//NOTE HOW TO CREATE A PROMISE :
+
+//NOTE for creating a promise we call a Promise constructor function after that we are going to handle its states ;
+
+const promise = new Promise((resolve, reject) => {
+  let success;
+  if (success) {
+    resolve('data');
+  } else {
+    reject('not found');
+  }
+});
+
+promise
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
+  .finally(() => console.log('this will run for both fulfilled and rejected'));
+
+//NOTE function which return directly a promise we donot have to create it , and used for api call ;
+
+//   fetch() => always return a promise you have to handle its states
+
+const result = fetch('https://jsonplaceholder.typicode.com/todos');
+
+result.then((response) => response.json()).then((data) => console.log(data));
