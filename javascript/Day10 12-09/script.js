8da6999a761747f6ce9d15d309e6a9e656293d5c
ritesh <riteshpatidar088@gmail.com>
2025-09-12 10:00:02 +0530

    updated till api call

--- javascript/Day10 12-09/script.js
+++ javascript/Day10 12-09/script.js
@@ -1,11 +1,18 @@
 const form = document.getElementById('form');
 const searchInput = document.getElementById('search');
-
+const btn = document.getElementById('btn');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const searchQuery = searchInput.value;
   console.log(searchQuery);
+  btn.textContent = 'Searching....';
   fetch(`http://www.omdbapi.com/?s=${searchQuery}&apikey=b0eec689`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+    .finally(() => {
+      btn.textContent = 'Search';
+    });
 });
