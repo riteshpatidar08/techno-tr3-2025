536beedc741138fa1c313766505ec42a414eb112
ritesh <riteshpatidar088@gmail.com>
2025-09-15 11:01:58 +0530

    added new code

---
+++ git.js
@@ -0,0 +1,16 @@
+
+//git init
+//git status
+//git add .
+//git commit -m "some message regarding changes"
+
+//adding remote repo 
+git remote add origin url_for_remote_repo
+
+git remote // to check the current remote repo 
+
+git push -u master //to push the changes on the master branch on remote repo.
+
+git checkout -b newBranch //to create a new branch on the local repo.
+
+git push origin newbranch //to push the changes you have done on the newbranch then push the branch to the remote repo.
